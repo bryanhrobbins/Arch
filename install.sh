5cb8525ea6873e5c5ddc341154058a07de7806be
Bryan Robbins <bryanhrobbins@gmail.com>
2026-04-12 14:58:59 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 source ./install.conf
 
-echo "--- Partitioning $DRIVE (1GiB EFI) ---"
+echo "--- Partitioning & Formatting ---"
 sgdisk -Z $DRIVE
-sgdisk -n 1:0:+1G   -t 1:ef00 $DRIVE
-sgdisk -n 2:0:0     -t 2:8300 $DRIVE
-
-echo "--- Formatting & Btrfs Setup ---"
+sgdisk -n 1:0:+1G -t 1:ef00 $DRIVE
+sgdisk -n 2:0:0   -t 2:8300 $DRIVE
 mkfs.fat -F 32 "${DRIVE}p1"
 mkfs.btrfs -L ARCH "${DRIVE}p2" -f
 
@@ -17,7 +15,7 @@ for sv in "${SUBVOLS[@]}"; do
 done
 umount /mnt
 
-echo "--- Mounting Hierarchy ---"
+echo "--- Mounting ---"
 mount -o compress=zstd,ssd,discard=async,subvol=@ "${DRIVE}p2" /mnt
 mkdir -p /mnt/{efi,home,var/log,.snapshots,boot}
 mount "${DRIVE}p1" /mnt/efi
@@ -27,26 +25,16 @@ mount -o compress=zstd,ssd,discard=async,subvol=@home "${DRIVE}p2" /mnt/home
 mount -o compress=zstd,ssd,discard=async,subvol=@var_log "${DRIVE}p2" /mnt/var/log
 mount -o compress=zstd,ssd,discard=async,subvol=@snapshots "${DRIVE}p2" /mnt/.snapshots
 
-echo "--- Installing Packages ---"
+echo "--- Base Install & Source Deploy ---"
 pacstrap -K /mnt "${PKGS[@]}"
-
-echo "--- Generating Fstab ---"
 genfstab -U /mnt >> /mnt/etc/fstab
+chmod +x ./src/etc/skel/first-run.sh
+chmod +x ./src/etc/skel/check-setup.sh
+cp -rv ./src/* /mnt/
 
-echo "--- Configuring systemd & zram ---"
-sed -i 's/^HOOKS=(.*)/HOOKS=(systemd autodetect modconf kms sd-vconsole block filesystems btrfs)/' /mnt/etc/mkinitcpio.conf
+sed -i 's|^SHELL=.*|SHELL=/usr/bin/zsh|' /mnt/etc/default/useradd
 
-cat <<zramEOF > /mnt/etc/systemd/zram-generator.conf
-[zram0]
-zram-size = ram / 2
-compression-algorithm = zstd
-swap-priority = 100
-fs-type = swap
-zramEOF
-
-echo "--- Entering Chroot ---"
 arch-chroot /mnt /bin/bash <<EOF
-# Localization & System
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
 echo "$LOCALE UTF-8" >> /etc/locale.gen
@@ -54,22 +42,15 @@ locale-gen
 echo "LANG=$LOCALE" > /etc/locale.conf
 echo "$HOSTNAME" > /etc/hostname
 echo "EDITOR=vi" >> /etc/environment
-echo "VISUAL=vi" >> /etc/environment
-
-# User & Sudo
-useradd -m -G wheel -s /bin/bash -c "$FULLNAME" $USERNAME
+chsh -s /usr/bin/zsh root
+useradd -m -G wheel -c "$FULLNAME" $USERNAME
 echo "Set password for $USERNAME:"
 passwd $USERNAME
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-# Networking
 systemctl enable NetworkManager
-
-# Initramfs & Bootloader
+systemctl enable iwd
 mkinitcpio -P
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 sed -i 's|^GRUB_CMDLINE_LINUX_DEFAULT=.*|GRUB_CMDLINE_LINUX_DEFAULT="quiet rw mem_sleep_default=s2idle rootflags=subvol=@"|' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 EOF
-
-echo "Installation complete! The system is ready for reboot."
